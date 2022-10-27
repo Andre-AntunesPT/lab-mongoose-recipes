5a7563e2270dc2c6b25886012d1b78424e068c1b
André Antunes <andre.n.antunes@gmail.com>
2022-10-27 15:20:07 +0100

    Solved Lab

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 //Method 1 : Using Async Await
 
@@ -19,6 +19,34 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
+    //2
+    const recipe = new Recipe({
+      title: "Ovos Mexidos",
+      level: "Easy Peasy",
+      ingredients: "Ovos, Sal, Pimenta",
+      cuisine: "Mexe os ovos e acrescenta sal e pimenta",
+      dishType: "breakfast",
+      image:
+        "https://feelgoodfoodie.net/wp-content/uploads/2022/02/How-to-Make-Scrambled-Eggs-08.jpg",
+      duration: 10,
+      creator: "Chef",
+    });
+    const savedRecipe = await recipe.save();
+    console.log(savedRecipe.title);
+    //3
+    const createAllRecipes = await Recipe.insertMany(data);
+    createAllRecipes.forEach((recipe) => console.log(recipe.title));
+    //4
+    const updatedRecipe = await Recipe.updateOne(
+      { duration: 220 },
+      { duration: 100 }
+    );
+    console.log("Recipe Updated");
+    //5
+    const removeRecipe = await Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log("Recipe Deleted");
+    //6
+    dbConnection.disconnect();
   } catch (error) {
     console.log(error);
   }
